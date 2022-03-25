ca1433ec4dca4f611f16c2f1456cc522400a694a
Bibash Shrestha <bibashshrestha@gmail.com>
2022-03-25 13:04:38 +0545

    finding a widget

--- test/calculator_app_test.dart
+++ test/calculator_app_test.dart
@@ -1,5 +1,6 @@
 import 'package:calculator_app_testing/main.dart';
 import 'package:calculator_app_testing/two_digit_operation.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -8,6 +9,39 @@ void main() {
         (WidgetTester tester) async {
       await tester.pumpWidget(const CalculatorApp());
       expect(find.byType(TwoDigitOperation), findsNWidgets(4));
+    });
+  });
+
+  group('add', () {
+    testWidgets('shows result when given two numbers', (tester) async {
+      await tester.pumpWidget(const CalculatorApp());
+      // await tester.enterText(
+      //   find.byKey(const Key('textField_top_plus')),
+      //   '3',
+      // );
+      // await tester.enterText(
+      //   find.byKey(const Key('textField_bottom_plus')),
+      //   '6',
+      // );
+
+      final topTextFieldFinder = find.byKey(const Key('textField_top_plus'));
+      final bottomTextFieldFinder =
+          find.byKey(const Key('textField_bottom_plus'));
+
+      //ensure that widget is visible in scrollable view
+      await tester.ensureVisible(topTextFieldFinder);
+      await tester.tap(topTextFieldFinder);
+      await tester.enterText(topTextFieldFinder, '3');
+
+      await tester.ensureVisible(bottomTextFieldFinder);
+      await tester.tap(bottomTextFieldFinder);
+      await tester.enterText(bottomTextFieldFinder, '6');
+
+      //Transitioning from 'is ???' to 'is 9.0' represents a new frame
+      //await tester.pump(); => only works with single frame | fails if there
+      //are more frames in 1 sec
+      await tester.pumpAndSettle();
+      expect(find.text('is 9.0'), findsOneWidget);
     });
   });
 }
