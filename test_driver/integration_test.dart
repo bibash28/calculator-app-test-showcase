0a29d73116a5eda9e33034bad1f8cb2426ab7f75
Bibash Shrestha <bibashshrestha@gmail.com>
2022-03-29 12:01:36 +0545

    screenshot saved

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,3 +1,18 @@
-import 'package:integration_test/integration_test_driver.dart';
+import 'dart:developer';
+import 'dart:io';
+import 'package:integration_test/integration_test_driver_extended.dart';
 
-Future<void> main() => integrationDriver();
+Future<void> main() async {
+  try {
+    await integrationDriver(
+      onScreenshot: (String screenshotName, List<int> screenshotBytes) async {
+        final image = await File('screenshots/$screenshotName.png')
+            .create(recursive: true);
+        image.writeAsBytesSync(screenshotBytes);
+        return true;
+      },
+    );
+  } catch (e) {
+    log('Error occurred: $e');
+  }
+}
